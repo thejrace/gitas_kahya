17f2e659ececff90c87ad4fccd351c7ee1cafbdc
JesterPC <ahmetkanbur@gmail.com>
2019-09-07 13:03:55 +0300

    refactor code, add comments and javadocs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+/*
+ *  Kahya - Gitas 2019
+ *
+ *  Contributors:
+ *      Ahmet Ziya Kanbur 2019-
+ *
+ * */
 public class Main {
-
     public static void main(String[] args){
-
-
-
-
     }
-
 }
